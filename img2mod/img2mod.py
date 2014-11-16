5a910b2e1574863017f56656c9709bdfcd205713
Michael Witrant <michael@ouvrages-web.fr>
2014-11-16 18:57:24 +0100

    imported previous img2mod version

--- img2mod/img2mod.py
+++ img2mod/img2mod.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 #
 # img2mod - A script to convert an image file into a PCB module file for Kicad.
-# Version 1.02 - June 19, 2013
-# Now works with the new .kicad_mod s-expression file format
+# Version 1.01 - November 21, 2010
 #
 # Written by Matthew Beckler for Wayne and Layne, LLC
 # Copyright (c) 2011, Wayne and Layne, LLC
@@ -14,42 +13,37 @@
 # (at your option) any later version.
 
 
-from PIL import Image, ImageOps
+from PIL import Image
 
 # Text for the file header, the parameter is the name of the module, ex "LOGO".
-header = """(module %(name)s
-  (layer F.Cu)
-  (at 0 0)
-  (fp_text reference "VAL***" (at 0 10) (layer F.SilkS) hide
-    (effects (font (thickness 0.3)))
-  )
-  (fp_text reference "%(name)s" (at 0 5) (layer F.SilkS) hide
-    (effects (font (thickness 0.3)))
-  )
+header = """PCBNEW-LibModule-V1
+$INDEX
+%(name)s
+$EndINDEX
+$MODULE %(name)s
+Po 0 0 0 15 00000000 00000000 ~~
+T0 0 2866 600 600 0 120 N I 21 "G***"
+T1 0 -2866 600 600 0 120 N I 21 "%(name)s"
 """
 
 # Text for the file footer, the only parameter is the name of the module
-footer = """)
+footer = """$EndMODULE %(name)s
+$EndLIBRARY
 """
 
 # Places a pixel with (x, y) at the upper-left corner
-# Size is in units of mm
+# Size is in units of 0.1 mil = 0.0001 inch
 def make_pixel(x, y, px_size):
-    return """  (fp_poly
-    (pts
-      (xy %(0)s %(1)s)
-      (xy %(2)s %(1)s)
-      (xy %(2)s %(3)s)
-      (xy %(0)s %(3)s)
-      (xy %(0)s %(1)s)
-    )
-    (layer F.SilkS)
-    (width 0.01)
-  )
+    return """DP 0 0 0 0 5 1 21
+Dl %(0)s %(1)s
+Dl %(2)s %(1)s
+Dl %(2)s %(3)s
+Dl %(0)s %(3)s
+Dl %(0)s %(1)s
 """ % {"0": x, "1": y, "2": x + px_size, "3": y + px_size}
 
 def conv_image_to_module(image, module_name, scale_factor):
-    """ Returns the text for the module, and the size: (x, y) in mm. """
+    """ Returns the text for the module, and the size: (x, y) in inches. """
     w, h = image.size
     #print "Original image dimensions: {0} x {1}".format(w, h)
     #print "Writing module file to \"{0}\"".format(output_filename)
@@ -60,7 +54,7 @@ def conv_image_to_module(image, module_name, scale_factor):
             if image.getpixel((x, y)) == 0:
                 module += make_pixel(scale_factor * x, scale_factor * y, scale_factor)
     module += footer % {"name": module_name}
-    return module, (scale_factor * w, scale_factor * h)
+    return module, ((scale_factor * w) / 10000.0, (scale_factor * h) / 10000.0)
 
 
 def main():
@@ -71,7 +65,7 @@ def main():
         print "  input_image is the filename of the input image"
         print "  output_filename is the name of the output module file"
         print "  module_name is the name to use for the module, traditionally LOGO"
-        print "  scale_factor is the size of each output pixel, in units of mm\""
+        print "  scale_factor is the size of each output pixel, in units of 0.1 mil = 0.0001\""
         sys.exit(1)
 
     input_image = sys.argv[1]
@@ -91,13 +85,13 @@ def main():
     #image = Image.composite(image, white, a)
 
     # If you want to invert the image:
-    #image = ImageOps.invert(image)
+    # image = PIL.ImageOps.invert(image)
 
     # If you want to do non-127 thresholding, change the 127 below
     image = image.point(lambda i: 0 if i < 127 else 255)
 
     module, size = conv_image_to_module(image, module_name, scale_factor)
-    print "Output image size: %f x %f mm" % (size[0], size[1])
+    print "Output image size: %f x %f inches" % (size[0], size[1])
 
     fid = open(output_filename, "w")
     fid.write(module)
